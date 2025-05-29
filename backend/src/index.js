0cca0d9fa7ffb42eed0cf4713ef1ff2e3d2a9ca8
Rajyavardhan Singh <srajyavardhan12@gmail.com>
2025-05-29 14:28:52 +0530

    checkadmin middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,6 +25,9 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/problems', problemRoutes)
+
+
 app.listen(process.env.PORT,()=>{
     console.log(`server running on port ${process.env.PORT}`)
 })
---
+++ backend/src/routes/problem.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+
+
+const problemRoutes = express.Router()
+
+problemRoutes.post("/create-problem" , authMiddleware,checkAdmin , createProblem)
+
+export default problemRoutes;
