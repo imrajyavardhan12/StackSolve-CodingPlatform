1b48cf71b214ef1cc156d60b146e691ddfbe7e31
Rajyavardhan Singh <srajyavardhan12@gmail.com>
2025-06-04 21:04:13 +0530

    add get all submission controller

--- backend/src/routes/submission.route.js
+++ backend/src/routes/submission.route.js
@@ -1,6 +1,8 @@
 import express from "express"
+
 import { authMiddleware } from "../middleware/auth.middleware.js";
-import { getAllSubmission, getAllTheSubmissionsForProblem, getSubmissionsForProblem } from "../controllers/submission.controller.js";
+
+import { getAllSubmission, getSubmissionsForProblem, getAllTheSubmissionsForProblem } from "../controller/submission.controller.js";
 
 
 const submissionRoutes = express.Router();
