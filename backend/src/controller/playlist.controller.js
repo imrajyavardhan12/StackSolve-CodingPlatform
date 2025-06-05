17ceb00350e3953391651d5bd7e0d976fe555e6d
Rajyavardhan Singh <srajyavardhan12@gmail.com>
2025-06-05 07:52:50 +0530

    add getPlayAllListDetails controller

--- backend/src/controller/playlist.controller.js
+++ backend/src/controller/playlist.controller.js
@@ -1,12 +1,33 @@
-import { db } from "../libs/db.js"
+import { db } from "../libs/db.js";
 
-export const getPlayAllListDetails = async (req,res) => {
+export const getPlayAllListDetails = async (req, res) => {
+  try {
+    const playLists = await db.playlist.findMany({
+      where: {
+        userId: req.user.id,
+      },
+      include: {
+        problems: {
+          include: {
+            problem: true,
+          },
+        },
+      },
+    });
+    res.status(200).json({
+      success: true,
+      message: "Playlist fetched successfully",
+      playLists,
+    });
+  } catch (error) {
+    console.error("Error fetching playlist:", error);
+    res.status(500).json({ error: "Failed to fetch playlist" });
+  }
+};
+export const getPlayListDetails = async (req, res) => {};
 
-
-}
-export const getPlayListDetails = async (req,res) => {}
-export const createPlayList = async (req,res) => {
-    try {
+export const createPlayList = async (req, res) => {
+  try {
     const { name, description } = req.body;
     const userId = req.user.id;
 
@@ -26,8 +47,7 @@ export const createPlayList = async (req,res) => {
     console.error("Error creating playlist:", error);
     res.status(500).json({ error: "Failed to create playlist" });
   }
-
-}
-export const addProblemToPlaylist = async (req,res) => {}
-export const deletePlayList = async (req,res) => {}
-export const removeProblemFromPlaylist = async (req,res) => {}
+};
+export const addProblemToPlaylist = async (req, res) => {};
+export const deletePlayList = async (req, res) => {};
+export const removeProblemFromPlaylist = async (req, res) => {};
