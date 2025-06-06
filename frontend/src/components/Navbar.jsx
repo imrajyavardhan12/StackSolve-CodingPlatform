998564e9f680abeb38b762f42b4cdd3c1949ecca
Rajyavardhan Singh <132651154+imrajyavardhan12@users.noreply.github.com>
2025-06-06 10:44:29 +0530

    remove logs

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,18 +4,13 @@ import { useAuthStore } from "../store/useAuthStore";
 import { Link } from "react-router-dom";
 import LogoutButton from "./LogoutButton";
 
-
-
 const Navbar = ()=>{
 
     const {authUser} = useAuthStore()
 
-    console.log("AUTH_USER",authUser)
-
     return (
      <nav className="sticky top-0 z-50 w-full py-5">
       <div className="flex w-full justify-between mx-auto max-w-4xl bg-black/15 shadow-lg shadow-neutral-600/5 backdrop-blur-lg border border-gray-200/10 p-4 rounded-2xl">
-        {/* Logo Section */}
         <Link to="/" className="flex items-center gap-3 cursor-pointer">
           <img src="/leetlab.svg" className="h-18 w-18 bg-primary/20 text-primary border-none px-2 py-2 rounded-full" />
           <span className="text-lg md:text-2xl font-bold tracking-tight text-white hidden md:block">
@@ -23,7 +18,6 @@ const Navbar = ()=>{
           </span>
         </Link>
 
-        {/* User Profile and Dropdown */}
         <div className="flex items-center gap-8">
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar flex flex-row ">
@@ -43,10 +37,6 @@ const Navbar = ()=>{
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 space-y-3"
             >
-              {/* Admin Option */}
-             
-
-              {/* Common Options */}
               <li>
                 <p className="text-base font-semibold">
                  
@@ -88,6 +78,5 @@ const Navbar = ()=>{
     </nav>
     )
 }
-
 
 export default Navbar;
